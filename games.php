83423d768e2df25b2430b971cd5deb6718eec5a5
Mohammed <malrasheidi@una.edu>
2024-11-08 01:44:13 -0600

    Update games.php

--- games.php
+++ games.php
@@ -7,17 +7,54 @@ if (isset($_SESSION['username'])) {
     header("location: index.php");
     exit;
 }
+
+$conn = new mysqli("localhost", "root", "", "softball");
+
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+$sql = "SELECT * FROM games";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+    ?>
+
+    <!DOCTYPE html>
+    <html>
+        <head>
+            <meta charset="UTF-8">
+            <title>Games</title>
+        </head>
+        <body>
+            <h1>Games List</h1>
+
+            <table border="1">
+                <tr>
+                    <th>Game ID</th>
+                    <th>Opponent</th>
+                    <th>Site</th>
+                    <th>Result</th>
+                </tr>
+
+                <?php
+                while ($row = $result->fetch_assoc()) {
+                    echo "<tr>";
+                    echo "<td>" . $row['id'] . "</td>";
+                    echo "<td>" . $row['opponent'] . "</td>";
+                    echo "<td>" . $row['site'] . "</td>";
+                    echo "<td>" . $row['result'] . "</td>";
+                    echo "</tr>";
+                }
+                ?>
+            </table>
+
+        </body>
+    </html>
+
+    <?php
+} else {
+    echo "No games found.";
+}
+$conn->close();
 ?>
-<!DOCTYPE html>
-<html>
-    <head>
-        <meta charset="UTF-8">
-        <title></title>
-    </head>
-    <body>
-        Display games here...
-        <?php
-        // put your code here
-        ?>
-    </body>
-</html>
